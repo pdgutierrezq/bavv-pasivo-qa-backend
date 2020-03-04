40d594af0f08c472b6aaa3b2e52fa87380fcd887
JhoanGarcia <jhoan.garcia@avaldigitallabs.com>
2020-03-04 17:35:04 -0500

    :tada: @PBA-15 identificacion de usuario generar otp

--- src/test/java/co/com/avvillaspasivos/stepsdefinitions/IdentificacionUsuarioStepsDefinitions.java
+++ src/test/java/co/com/avvillaspasivos/stepsdefinitions/IdentificacionUsuarioStepsDefinitions.java
@@ -9,22 +9,40 @@
 package co.com.avvillaspasivos.stepsdefinitions;
 
 import co.com.avvillaspasivos.data.DataProvider;
+import co.com.avvillaspasivos.model.BodyGenerarOtp;
+import co.com.avvillaspasivos.paths.ServicePaths;
+import co.com.avvillaspasivos.tasks.CallPost;
+import co.com.avvillaspasivos.tasks.ValidarIdentificacionUsuario;
+import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 
 public class IdentificacionUsuarioStepsDefinitions {
+private BodyGenerarOtp bodyGenerarOtp=BodyGenerarOtp.builder().build();
 
-  @Dado("que tengo el {string}, {string} de documento y demas datos de autenticacion de un usuario")
-  public void queTengoElDeDocumentoYDemasDatosDeAutenticacionDeUnUsuario(String arg0, String arg1) {
-    DataProvider.getUsersIdentification().forEach(System.out::println);
-  }
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @Dado("que tengo datos de autenticacion de un usuario {string}")
+    public void queTengoDatosDeAutenticacionDeUnUsuario(String tipoUsuario) {
+        OnStage.theActorCalled(tipoUsuario).whoCan(CallAnApi.at(ServicePaths.getEndPointBase()));
+        bodyGenerarOtp=DataProvider.getBodyIdentification(tipoUsuario);
+    }
 
   @Cuando("consumo el servicio rest de identificacion")
-  public void consumoElServicioRestDeIdentificacion() {}
+  public void consumoElServicioRestDeIdentificacion() {
+      OnStage.theActorInTheSpotlight()
+          .attemptsTo(CallPost.pathBody(ServicePaths.pathUserIdentity(), bodyGenerarOtp));
+  }
 
-  @Entonces(
-      "el servicio se encarga de enviar OTP al usuario o me informa que no paso alguna validacion")
-  public void elServicioSeEncargaDeEnviarOTPAlUsuarioOMeInformaQueNoPasoAlgunaValidacion() {}
-
+    @Entonces("el servicio se encarga de enviar OTP al usuario {string} o me informa que no paso alguna validacion")
+    public void elServicioSeEncargaDeEnviarOTPAlUsuarioOMeInformaQueNoPasoAlgunaValidacion(String tipoUsuario) {
+        ValidarIdentificacionUsuario.tipo(tipoUsuario);
+    }
 }
---
+++ src/test/resources/features/autenticacion/condicionesClienteWs.feature
@@ -0,0 +1,40 @@
+# language: es
+@FBackend:test
+
+
+Característica: condiciones cliente
+  YO COMO sistema
+  NECESITO on¡btener informaicon de las condicion es del cliente
+  PARA direccionarlo al flujo correspondiente
+
+  @Sprint-3
+  Esquema del escenario: Consumir servicio de condiciones cliente
+    Dado que tengo un usuario tipo "<usuario>"
+    Cuando consumo el servicio rest de condiciones cliente
+    Entonces el obtengo la informacion de las condiciones del cliente
+    Ejemplos:
+      | usuario             |
+      | listas restrictivas |
+
+#
+#  @example
+#  Esquema del escenario: test flujo
+#    Dado el usuario "<tipo usuario>" diligencia el formulario  de identificacion
+#    Cuando esta en pantalla de ofecimiento de cuenta y selecciona cuenta "<tipo cuenta>"
+#    Y "<aceptacion seguro>" acepta el ofrecimiento de seguro
+#    Entonces el usuario ingresa la otp
+#    Ejemplos:
+#      | tipo usuario | tipo cuenta | aceptacion seguro |
+#      | actualizado  | simple      | no                |
+#  @example
+#  Escenario: test obtener otp
+#    Dado que se genera otp
+#    Cuando se consulta massivian
+#    Entonces se obtiene otp
+
+
+#  @example
+#  Escenario: test obtener otp
+#    Dado que se conecta a oracle
+#    Cuando se consulta massivian
+#    Entonces se obtiene otp
